6c9fd1f604c791a05af20a25033b184f5e57a6f3
Shawon Roy <skrshawon@gmail.com>
2020-07-17 21:38:52 +0600

    feet to mile

---
+++ assigment.js
@@ -0,0 +1,21 @@
+//feetToMile
+
+function feetToMile(mile){
+    var feet = mile * 0.00018939;
+    return feet;
+}
+
+// var result = feetToMile(60000);
+// console.log(result);
+
+
+//woodCalculator
+
+function woodCalculator(){
+    
+}
+
+
+//brickCalculator
+
+//tinyFriend
